d85190db86b81aca980bab417a2b36045019441c
Jekson365 <jemali.ergemlidze.1@btu.edu.ge>
2024-08-04 22:55:01 +0400

    kk

--- app/controllers/sell_types_controller.rb
+++ app/controllers/sell_types_controller.rb
@@ -3,4 +3,14 @@ class SellTypesController < ApplicationController
     sell_types = SellType.all
     render json: sell_types
   end
+  def create
+    sell_type = SellType.new(sell_type_params)
+    if sell_type.save
+      render json: sell_type
+    end
+  end
+  private
+  def sell_type_params
+    params.permit(:name)
+  end
 end
